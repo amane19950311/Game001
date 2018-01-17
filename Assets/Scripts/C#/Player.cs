b26b0dd17e6140d875fb5a83812fbf8c1a658cac
hashimotoamane <hashimoto.amane0311@gmail.com>
2018-01-17 20:39:26 +0900

    モーション修正、ガード処理、ジャンプ処理途中

--- Assets/Scripts/C#/Player.cs
+++ Assets/Scripts/C#/Player.cs
@@ -13,11 +13,21 @@ public class Player: MonoBehaviour {
 	private bool jumpFlg = false;
 
 	[SerializeField]
-	private float jumpHeight = 10.0f;
+	GameObject guard = null;
+
+	private bool guardFlg = false;
+
+	private Rigidbody rb;
+
+	[SerializeField]
+	private float VELOCITY_RESET = 1;
+	private float velocity = 1;
+
+	float GRAVITY = -0.098f;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
@@ -27,15 +37,18 @@ public class Player: MonoBehaviour {
 
 		Action ();		//アクション処理
 
-		if(!jumpFlg)
-		{
+		//if(!jumpFlg)
+		//{
 			Jump ();		//ジャンプ処理
-		}
+		//}
 
-		if (transform.position.y < 0.0f) {
-			transform.position = new Vector3 (transform.position.x, 0.0f, 4);
-			jumpFlg = false;
-		}
+		//transform.position += new Vector3 (0.0f, -0.98f, 0.0f);
+
+		if (transform.position.y < -0.1f) {
+			transform.position = new Vector3 (transform.position.x, -0.1f, 1.0f);
+		} 
+
+		//rb.AddForce(new Vector3(0f,-9.8f,0f));
 		//transform.rotation = new Quaternion (transform.rotation.x,1f, transform.rotation.z,1.0f);
 	}
 
@@ -44,6 +57,7 @@ public class Player: MonoBehaviour {
 	/// </summary>
 	private void Move()
 	{
+		//コントローラ処理
 		/*if (Input.GetAxis ("Vertical")) {
 
 		}
@@ -52,6 +66,7 @@ public class Player: MonoBehaviour {
 
 		}*/
 
+		//キーボード処理
 		if (Input.GetKey (KeyCode.A)) {
 			transform.position += new Vector3 (-1.0f, 0, 0) * move;
 		} else if (Input.GetKey (KeyCode.D)) {
@@ -66,8 +81,20 @@ public class Player: MonoBehaviour {
 	/// </summary>
 	private void Action()
 	{
-		anim.SetBool ("Jub", Input.GetKey (KeyCode.J) || Input.GetButtonDown ("Action1"));
-		anim.SetBool ("HiKick", Input.GetKey (KeyCode.I) || Input.GetButtonDown ("Action2"));
+		//アクション処理
+		if (!guardFlg) {
+			anim.SetBool ("Jub", Input.GetKey (KeyCode.J) || Input.GetButtonDown ("Action1"));
+			anim.SetBool ("HiKick", Input.GetKey (KeyCode.I) || Input.GetButtonDown ("Action2"));
+		}
+
+		//ガード処理
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetButtonDown ("Action2")) {
+			guardFlg = true;
+		} else {
+			guardFlg = false;
+		}
+
+		guard.SetActive (guardFlg);
 	}
 
 	/// <summary>
@@ -75,13 +102,29 @@ public class Player: MonoBehaviour {
 	/// </summary>
 	private void Jump()
 	{
-		if (Input.GetKey (KeyCode.Space) || Input.GetButtonDown ("Jump")) {
-			jumpFlg = true;
+		if (!jumpFlg) {
+			if (Input.GetKey (KeyCode.Space) || Input.GetButtonDown ("Jump")) {
+				jumpFlg = true;
+			}
 		}
-
 		if (jumpFlg) {
-			transform.position += new Vector3 (0.0f, jumpHeight, 0.0f);
+			transform.position += new Vector3(0f,velocity,0f);
+
+			velocity += GRAVITY;
+
+		}			
+	}
+
+	/// <summary>
+	/// 地面との衝突判定
+	/// </summary>
+	/// <param name="col">Col.</param>
+	private void OnCollisionEnter(Collision col)
+	{
+		//地面に着地
+		if (col.transform.tag == "Field") {
+			jumpFlg = false;
+			velocity = VELOCITY_RESET;
 		}
-			
 	}
 }
---
+++ Assets/Scripts/Shader/OutlineToon.shader
@@ -0,0 +1,123 @@
+﻿//****************************************************
+// アウトライントゥーンシェーダ
+//****************************************************
+Shader "Custom/OutlineToon" {
+	Properties{
+	//使用宣言
+		_Color("Color", Color) = (1,1,1,1)
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_RampTex("Ramp", 2D) = "white"{}
+		_OutlineColor("Outline Color", Color) = (0,0,0,0)
+		_Outline("Outline",Range(0.001,0.005)) = 0.001
+	}
+	SubShader{
+		Tags{ "RenderType" = "Transparent" 
+			"Queue" = "Transparent"}
+		LOD 200
+
+		//アウトラインシェーダ
+		Pass{
+			Cull Front	//裏面描画
+
+			Stencil{
+				Ref 2			//ステンシル値
+				Comp always		//常にステンシルテストを成功
+				Pass Replace	//常にステンシルに書き込み
+			}
+			CGPROGRAM	//開始
+
+			#include "UnityCG.cginc"	//インクルード
+
+			#pragma vertex vert			//頂点シェーダ宣言
+			#pragma fragment frag		//フラグメントシェーダ宣言
+
+			float _Outline;				//アウトライン
+			fixed4 _OutlineColor;		//アウトラインカラー
+
+			//取得情報
+			struct appdata {
+				float4 position : POSITION;		//頂点座標
+				float3 normal : NORMAL;			//法線
+			};
+
+			//出力情報
+			struct v2f {
+				float4 position : SV_POSITION;	//頂点座標
+				fixed4 color : COLOR0;			//カラー
+			};
+
+			//頂点シェーダ
+			v2f vert(appdata i)
+			{
+				float distance = -UnityObjectToViewPos(i.position).z;	//カメラクリップ空間へ変換（モデルと被った部分は見えなくさせるための変換）
+				i.position.xyz += i.normal * distance * _Outline;		//モデルを法線の方向へ拡大する
+
+				v2f o;
+				o.position = UnityObjectToClipPos(i.position);	//座標変換
+				o.color = 0;
+
+				return o;
+			}
+
+			//フラグメントシェーダ
+			fixed4 frag(v2f o) : SV_Target
+			{
+				o.color = _OutlineColor;	//カラー代入
+
+				return o.color;
+			}
+
+			ENDCG	//終了
+		}
+
+
+		//トゥーンシェーダ
+		Stencil{
+			Ref 3			//ステンシル値
+			Comp Always		//常にステンシルテストを成功
+			Pass Replace	//常にステンシルに書き込み
+		}
+
+		CGPROGRAM	//開始
+
+		#pragma surface surf ToonRamp		//ライト情報が必要なため、サーフェースシェーダを使用
+		#pragma target 3.0
+
+		sampler2D _MainTex;		//メインテクスチャ
+		sampler2D _RampTex;		//トゥーンテクスチャ
+
+		//出力
+		struct Input
+		{
+			float2 uv_MainTex;	//テクスチャ座標	
+		};
+
+		fixed4 _Color;	//カラー
+
+		//ライティング用メソッド
+		fixed4 LightingToonRamp(SurfaceOutput s, fixed3 lightDir, fixed atten)
+		{
+			//法線とライトの方向ベクトルの内積値(0.0f～1.0f)
+			half d = dot(s.Normal, lightDir) * 0.5f + 0.5f;
+
+			//値に応じた明度のUV座標に設定
+			fixed3 ramp = tex2D(_RampTex, fixed2(d,0.5f)).rgb;
+
+			//色の計算
+			fixed4 c;
+			c.rgb = s.Albedo * _LightColor0.rgb * ramp;
+			c.a = 0;
+			return c;
+		}
+
+		//サーフェースシェーダ
+		void surf(Input IN, inout SurfaceOutput o) {
+
+			fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;		//テクスチャとカラーを乗算合成
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+		ENDCG	//終了
+
+	}
+}
